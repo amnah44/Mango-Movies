87c39d21a54a8e56da838720f9cd78e49a919856
Amnah Ali <amnah1li@gmail.com>
2021-10-28 22:07:43 +0300

    add Model package

--- app/src/main/java/com/mango/movies/ui/MovieActivity.kt
+++ app/src/main/java/com/mango/movies/ui/MovieActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mango.movies.R
 
-class MainActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie)
     }
 }
