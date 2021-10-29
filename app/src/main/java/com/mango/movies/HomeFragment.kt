de0276dcbdbaacf9cac87df40ef7cadec2f99872
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-29 19:15:04 +0300

    create home fragment

---
+++ app/src/main/java/com/mango/movies/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.mango.movies
+
+import com.mango.movies.databinding.FragmentHomeBinding
+import com.mango.movies.ui.base.BaseFragment
+import com.mango.movies.util.Constant
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override val LOG_TAG: String = Constant.HOME_FRAGMENT
+
+    override fun setupView() {
+
+    }
+}
