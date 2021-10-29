910cb62c257f7063c8e004dde6e9995b024c0ed0
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-29 19:20:56 +0300

    create details fragment

---
+++ app/src/main/java/com/mango/movies/DetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.mango.movies
+
+import com.mango.movies.databinding.FragmentDetailsBinding
+import com.mango.movies.ui.base.BaseFragment
+import com.mango.movies.util.Constant
+
+
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(R.layout.fragment_details) {
+    override val LOG_TAG: String = Constant.DETAILS_FRAGMENT
+
+    override fun setupView() {}
+}
