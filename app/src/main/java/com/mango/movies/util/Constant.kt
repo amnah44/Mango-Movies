6a77e429403834fadc0beb31d329ca226913a280
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-29 19:05:49 +0300

    add fragments log tags as constants

--- app/src/main/java/com/mango/movies/util/Constant.kt
+++ app/src/main/java/com/mango/movies/util/Constant.kt
@@ -1,5 +1,14 @@
 package com.mango.movies.util
 
 object Constant {
-
+    //Log tags to be used inside the fragments classes
+    const val HOME_FRAGMENT = "Home Fragment"
+    const val DETAILS_FRAGMENT = "Details Fragment"
+    const val PEOPLE_FRAGMENT = "People Fragment"
+    const val ACTOR_DETAILS_FRAGMENT = "Actor Details Fragment"
+    const val SHOW_MORE_MOVIES_FRAGMENT = "Show More Movies Fragment"
+    const val SHOW_MORE_SERIES_FRAGMENT = "Show More Series Fragment"
+    const val FAVORITES_FRAGMENT = "Favorites Fragment"
+    const val SEARCH_FRAGMENT = "Search Fragment"
+    const val SETTINGS_FRAGMENT = "Settings Fragment"
 }
