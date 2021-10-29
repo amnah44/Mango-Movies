4850a1c2fdfb27da88a16542bc3399c32f3bcf13
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-29 19:51:44 +0300

    create search fragment

---
+++ app/src/main/java/com/mango/movies/SearchFragment.kt
@@ -0,0 +1,12 @@
+package com.mango.movies
+
+import com.mango.movies.databinding.FragmentSearchBinding
+import com.mango.movies.ui.base.BaseFragment
+import com.mango.movies.util.Constant
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override val LOG_TAG: String = Constant.SEARCH_FRAGMENT
+
+    override fun setupView() {
+    }
+}
