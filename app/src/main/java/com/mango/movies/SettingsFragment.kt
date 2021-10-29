81fbde44985b427777eb275c9f8697589962cc79
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-29 19:55:05 +0300

    create settings fragment

---
+++ app/src/main/java/com/mango/movies/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.mango.movies
+
+import com.mango.movies.databinding.FragmentSettingsBinding
+import com.mango.movies.ui.base.BaseFragment
+import com.mango.movies.util.Constant
+
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
+    override val LOG_TAG: String = Constant.SETTINGS_FRAGMENT
+
+    override fun setupView() {
+    }
+}
