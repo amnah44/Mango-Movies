a844394f211c8c13cd43127bb1653374616f4dca
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-10-30 14:01:27 +0300

    rename binding adapter

--- app/src/main/java/com/mango/movies/util/BindingAdapter.kt
+++ app/src/main/java/com/mango/movies/util/BindingAdapter.kt
@@ -3,24 +3,24 @@ package com.mango.movies.util
 import android.view.View
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter(value = ["app:showWhenLoading"])
-fun <T>showWhenLoading(view: View, state: State<T>?){
+@BindingAdapter(value = ["app:showOnLoading"])
+fun <T>showOnLoading(view: View, state: State<T>?){
     if (state is State.Loading)
         view.visibility= View.VISIBLE
     else
         view.visibility= View.GONE
 }
 
-@BindingAdapter(value = ["app:showWhenError"])
-fun <T>showWhenError(view: View, state: State<T>?){
+@BindingAdapter(value = ["app:showOnError"])
+fun <T>showOnError(view: View, state: State<T>?){
     if (state is State.Error)
         view.visibility= View.VISIBLE
     else
         view.visibility= View.GONE
 }
 
-@BindingAdapter(value = ["app:showWhenSuccess"])
-fun <T>showWhenSuccess(view: View, state: State<T>?){
+@BindingAdapter(value = ["app:showOnSuccess"])
+fun <T>showOnSuccess(view: View, state: State<T>?){
     if (state is State.Success)
         view.visibility= View.VISIBLE
     else
