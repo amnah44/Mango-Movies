d550f249df11b933d997a29da7ddb3e681dc5c35
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-10-30 21:11:12 +0300

    add binding adapter for set recycler items

--- app/src/main/java/com/mango/movies/util/BindingAdapter.kt
+++ app/src/main/java/com/mango/movies/util/BindingAdapter.kt
@@ -1,2 +1,14 @@
 package com.mango.movies.util
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.mango.movies.ui.base.BaseAdapter
+
+@BindingAdapter(value = ["app:items"])
+fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
+    if (items != null) {
+        (view.adapter as BaseAdapter<T>?)?.setItems(items)
+    } else {
+        (view.adapter as BaseAdapter<T>?)?.setItems(emptyList())
+    }
+}
