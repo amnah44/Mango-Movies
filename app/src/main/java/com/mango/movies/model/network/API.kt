abc3037bf9ee3971a797e31c7a0a9e575928e1a9
Amnah Ali <amnah1li@gmail.com>
2021-11-01 01:10:03 +0300

    add login interceptor to retrofit

--- app/src/main/java/com/mango/movies/model/network/API.kt
+++ app/src/main/java/com/mango/movies/model/network/API.kt
@@ -1,13 +1,22 @@
 package com.mango.movies.model.network
 
+import com.mango.movies.util.Constant.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object API {
-    private val BASE_URL="https://api.themoviedb.org/3/"
-    private val retrofit= Retrofit.Builder()
+    private val logging = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    private val myClient = OkHttpClient.Builder().addInterceptor(logging).build()
+
+    private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(myClient)
         .build()
 
     val apiService: MovieApiService = retrofit.create(MovieApiService::class.java)
--- app/src/main/java/com/mango/movies/util/Constant.kt
+++ app/src/main/java/com/mango/movies/util/Constant.kt
@@ -1,12 +1,13 @@
 package com.mango.movies.util
 
 object Constant {
-    const val api_key="68ff47a440b5f644e1533eb5afbd1669"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val api_key = "68ff47a440b5f644e1533eb5afbd1669"
+
     //Log tags to be used inside the fragments classes
     const val HOME_FRAGMENT = "Home Fragment"
     const val DETAILS_FRAGMENT = "Details Fragment"
     const val PEOPLE_FRAGMENT = "People Fragment"
-    const val ACTOR_DETAILS_FRAGMENT = "Actor Details Fragment"
     const val SHOW_MORE_MOVIES_FRAGMENT = "Show More Movies Fragment"
     const val SHOW_MORE_SERIES_FRAGMENT = "Show More Series Fragment"
     const val FAVORITES_FRAGMENT = "Favorites Fragment"
