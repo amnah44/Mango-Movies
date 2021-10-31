5cbd57f140af426f0e2d416032331b25fda43fa9
Amnah Ali <amnah1li@gmail.com>
2021-11-01 02:09:27 +0300

    add view model and binding in details fragment

--- app/src/main/java/com/mango/movies/ui/actorDetails/ActorDetailsFragment.kt
+++ app/src/main/java/com/mango/movies/ui/actorDetails/ActorDetailsFragment.kt
@@ -1,14 +1,24 @@
 package com.mango.movies.ui.actorDetails
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.mango.movies.R
 import com.mango.movies.databinding.FragmentActorDetailsBinding
 import com.mango.movies.ui.base.BaseFragment
+import com.mango.movies.ui.details.DetailsViewModel
 import com.mango.movies.util.Constant
 
-class ActorDetailsFragment : BaseFragment<FragmentActorDetailsBinding>(R.layout.fragment_actor_details) {
+class ActorDetailsFragment :
+    BaseFragment<FragmentActorDetailsBinding>(R.layout.fragment_actor_details) {
+    override val viewModel: DetailsViewModel by viewModels()
     override val LOG_TAG: String = Constant.ACTOR_DETAILS_FRAGMENT
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentActorDetailsBinding =
+        DataBindingUtil::inflate
 
     override fun setupView() {
     }
+
 
 }
