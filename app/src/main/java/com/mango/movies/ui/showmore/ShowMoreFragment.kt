9b4bdc38be73b18a409a504e5a528fc8b0a9007d
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-10-31 23:54:53 +0300

    refactor show more movie fragment and its xml file

--- app/src/main/java/com/mango/movies/ui/showmore/ShowMoreFragment.kt
+++ app/src/main/java/com/mango/movies/ui/showmore/ShowMoreFragment.kt
@@ -1,12 +1,12 @@
-package com.mango.movies.ui.movies
+package com.mango.movies.ui.showmore
 
 import com.mango.movies.R
-import com.mango.movies.databinding.FragmentShowMoreMoviesBinding
+import com.mango.movies.databinding.FragmentShowMoreBinding
 import com.mango.movies.ui.base.BaseFragment
 import com.mango.movies.util.Constant
 
-class ShowMoreMoviesFragment :
-    BaseFragment<FragmentShowMoreMoviesBinding>(R.layout.fragment_show_more_movies) {
+class ShowMoreFragment :
+    BaseFragment<FragmentShowMoreBinding>(R.layout.fragment_show_more) {
     override val LOG_TAG: String = Constant.SHOW_MORE_MOVIES_FRAGMENT
 
     override fun setupView() {
