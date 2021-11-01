bcfab042a36d052c359cf8f33a5e9b0e8b5df38e
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-02 02:02:59 +0300

    active recycler view

--- app/src/main/java/com/mango/movies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeFragment.kt
@@ -12,9 +12,11 @@ import com.mango.movies.util.Constant
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override val LOG_TAG: String = Constant.HOME_FRAGMENT
     override val viewModel: HomeViewModel by viewModels()
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentHomeBinding = DataBindingUtil::inflate
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentHomeBinding =
+        DataBindingUtil::inflate
 
     override fun setupView() {
-
+        binding.homeViewModel = viewModel
+        binding.categoryRecycler.adapter = HomeAdapter(mutableListOf(), viewModel)
     }
 }
