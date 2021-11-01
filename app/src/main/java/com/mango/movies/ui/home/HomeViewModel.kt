559921534b067669c1d4999168d8f3b8004f0719
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-01 18:20:06 +0300

    add genres as live data in view model

--- app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
@@ -1,7 +1,9 @@
 package com.mango.movies.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.mango.movies.model.repositiory.MovieRepository
 
 class HomeViewModel : ViewModel() {
-
+    val genres = MovieRepository.genres().asLiveData()
 }
