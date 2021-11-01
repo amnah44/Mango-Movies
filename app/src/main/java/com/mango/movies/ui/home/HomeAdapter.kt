b08f12f0e1bc5a50424b7ee3da2e6f510d8a2fa7
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-02 01:57:49 +0300

    movie home interaction listener to home adapter

--- app/src/main/java/com/mango/movies/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeAdapter.kt
@@ -3,10 +3,15 @@ package com.mango.movies.ui.home
 import com.mango.movies.R
 import com.mango.movies.model.domain.genre.Genre
 import com.mango.movies.ui.base.BaseAdapter
+import com.mango.movies.ui.base.BaseInteractionListener
 
-class HomeAdapter (
+class HomeAdapter(
     item: List<Genre>,
     listener: HomeInteractionListener
-): BaseAdapter<Genre>(item, listener) {
+) : BaseAdapter<Genre>(item, listener) {
     override val layoutId: Int = R.layout.item_genre
+}
+
+interface HomeInteractionListener : BaseInteractionListener {
+    fun onClickCategory(genre: Genre)
 }
--- app/src/main/java/com/mango/movies/ui/home/HomeInteractionListener.kt
+++
@@ -1,8 +0,0 @@
-package com.mango.movies.ui.home
-
-import com.mango.movies.model.domain.genre.Genre
-import com.mango.movies.ui.base.BaseInteractionListener
-
-interface HomeInteractionListener: BaseInteractionListener {
-    fun onClickCategory(genre: Genre)
-}
