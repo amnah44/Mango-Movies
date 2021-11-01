5500d3be8c7f86f2e0daba5e29f8b20bba5af3a5
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-02 01:53:28 +0300

    add repository as constraint in home view model

--- app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.asLiveData
 import com.mango.movies.model.domain.genre.Genre
 import com.mango.movies.model.repositiory.MovieRepository
 
-class HomeViewModel : ViewModel(), HomeInteractionListener {
-    val genres = MovieRepository.genres().asLiveData()
+class HomeViewModel(repository: MovieRepository) : ViewModel(), HomeInteractionListener {
+    val genres = repository.genres().asLiveData()
 
     override fun onClickCategory(genre: Genre) {
 
