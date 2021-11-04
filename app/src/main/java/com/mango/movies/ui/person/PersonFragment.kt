a3b1251442b3e7f405b4aede385fbec7129bd55c
Amnah Ali <amnah1li@gmail.com>
2021-11-04 21:01:50 +0300

    fix name

--- app/src/main/java/com/mango/movies/ui/person/PersonFragment.kt
+++ app/src/main/java/com/mango/movies/ui/person/PersonFragment.kt
@@ -4,15 +4,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.mango.movies.R
-import com.mango.movies.databinding.FragmentPeopleBinding
+import com.mango.movies.databinding.FragmentPersonBinding
 import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.ui.base.BaseFragment
 import com.mango.movies.util.Constant
 
-class PersonFragment : BaseFragment<FragmentPeopleBinding>(R.layout.fragment_people) {
+class PersonFragment : BaseFragment<FragmentPersonBinding>(R.layout.fragment_person) {
     override val LOG_TAG: String = Constant.PEOPLE_FRAGMENT
     override val viewModel = PersonPopularViewModel(repository = MovieRepository)
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentPeopleBinding = DataBindingUtil::inflate
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentPersonBinding = DataBindingUtil::inflate
 
     override fun setupView() {
         binding.personViewModel = viewModel
