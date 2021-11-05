c087864aa4830542e67f339b21bbec5caf62a911
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-05 13:43:07 +0300

    refactor Person adapter

--- app/src/main/java/com/mango/movies/ui/person/PersonAdapter.kt
+++ app/src/main/java/com/mango/movies/ui/person/PersonAdapter.kt
@@ -8,15 +8,4 @@ import com.mango.movies.model.domain.person.popular.PersonPopularResult
 class PersonAdapter(items: List<PersonPopularResult>, listener: PersonInteractionListener) :
     BaseAdapter<PersonPopularResult>(items, listener) {
     override val layoutId = R.layout.item_people
-
-    override fun onBindViewHolder(
-        holder: BaseViewHolder,
-        position: Int,
-        payloads: MutableList<Any>
-    ) {
-        super.onBindViewHolder(holder, position, payloads)
-        holder.itemView.setOnClickListener{v ->
-            Navigation.findNavController(v).navigate(R.id.action_personFragment_to_personDetailsFragment)
-        }
-    }
 }
