b491b6e56c0e29a7ff93052615cd31b3f82a0473
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-05 19:34:48 +0300

    add personImage URL

--- app/src/main/java/com/mango/movies/util/Constant.kt
+++ app/src/main/java/com/mango/movies/util/Constant.kt
@@ -3,6 +3,7 @@ package com.mango.movies.util
 object Constant {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_MOVIE_URL = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"
+    const val BASE_PERSON_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
     const val api_key = "68ff47a440b5f644e1533eb5afbd1669"
 
     //Log tags to be used inside the fragments classes
