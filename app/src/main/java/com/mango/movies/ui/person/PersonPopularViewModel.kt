7c268ad06ef49ea3a328733fe648a40a3023a553
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-05 13:43:37 +0300

    refactor PersonPopularViewModel

--- app/src/main/java/com/mango/movies/ui/person/PersonPopularViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/person/PersonPopularViewModel.kt
@@ -1,13 +1,15 @@
 package com.mango.movies.ui.person
 
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavArgs
+import androidx.navigation.Navigation
+import com.mango.movies.R
 import com.mango.movies.model.domain.movie.details.MovieDetailsResponse
-
 import kotlinx.coroutines.flow.collect
 import com.mango.movies.model.domain.person.details.PersonDetailsResponse
 import com.mango.movies.model.domain.person.popular.KnownFor
@@ -15,16 +17,26 @@ import com.mango.movies.model.domain.person.popular.PersonPopularResponse
 import com.mango.movies.model.domain.person.popular.PersonPopularResult
 import com.mango.movies.model.domain.tv.details.TvDetailsResponse
 import com.mango.movies.model.repositiory.MovieRepository
+import com.mango.movies.ui.home.HomeFragmentDirections
+import com.mango.movies.ui.home.HomeInteractionListener
 import com.mango.movies.ui.person.details.KnownForInteractionListener
 import com.mango.movies.util.State
 import kotlinx.coroutines.launch
 
-class PersonPopularViewModel(val repository: MovieRepository) : ViewModel(),PersonInteractionListener,KnownForInteractionListener{
-    val person = repository.personPopular().asLiveData()
-    val personDetails = MutableLiveData<PersonPopularResult?>()
+
+class PersonPopularViewModel : ViewModel(),
+    PersonInteractionListener, KnownForInteractionListener {
+    val person = MovieRepository.personPopular().asLiveData()
+//    val personDetails = MutableLiveData<PersonPopularResult?>()
+
 
     override fun onClickPerson(person: PersonPopularResult) {
-        personDetails.postValue(person)
+        Log.v("Ali", "onClickPerson")
+        Log.v("Ali", person.toString())
+        Log.v("Ali", person.name.toString())
+
+//        PersonFragmentDirections.actionPersonFragmentToPersonDetailsFragment(person)
+//        personDetails.postValue(person)
     }
 
     override fun getKnownFor(movieDetails: MovieDetailsResponse?, tvDetails: TvDetailsResponse?) {
