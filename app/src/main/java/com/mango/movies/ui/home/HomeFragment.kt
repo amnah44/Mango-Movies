ecd77d010d99c9f5d3868773669918352313cebe
Amnah Ali <amnah1li@gmail.com>
2021-11-05 08:41:14 +0300

    now playing movie is done

--- app/src/main/java/com/mango/movies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeFragment.kt
@@ -4,19 +4,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.mango.movies.R
 import com.mango.movies.databinding.FragmentHomeBinding
-import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.ui.base.BaseFragment
 import com.mango.movies.util.Constant
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override val LOG_TAG: String = Constant.HOME_FRAGMENT
-
-    //    override val viewModel: HomeViewModel by viewModels()
-    override val viewModel = HomeViewModel(repository = MovieRepository)
-
+    override val viewModel: HomeViewModel by viewModels()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentHomeBinding =
         DataBindingUtil::inflate
 
@@ -29,9 +26,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 //            val action = HomeFragmentDirections.actionHomeFragmentToCategoryFragment4(requireNotNull(it.id))
 //            Navigation.findNavController(binding.categoryRecycler).navigate(action)
 //        }
-        viewModel.nowPlayingMovies.observe(viewLifecycleOwner){
-            Log.i("Home", it.toData().toString())
-        }
         binding.searchView.setOnClickListener { view ->
             val action = HomeFragmentDirections.actionHomeFragmentToSearchFragment()
             Navigation.findNavController(view).navigate(action)
--- app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
@@ -3,18 +3,19 @@ package com.mango.movies.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.mango.movies.model.domain.movie.nowPlaying.MovieNowPlayingResult
+import com.mango.movies.model.domain.movie.topRated.MovieTopRatedResult
 import com.mango.movies.model.repositiory.MovieRepository
 
-class HomeViewModel(val repository: MovieRepository) : ViewModel(), HomeInteractionListener {
+class HomeViewModel() : ViewModel(), HomeInteractionListener {
 //    private val repository = MovieRepository
 //    val genres = MovieRepository.genres().asLiveData()
 //    val genreId = MutableLiveData<Genre>()
 
-    val nowPlayingMovies = repository.popularMovies().asLiveData()
-
+    val nowPlayingMovies = MovieRepository.nowPlayingMovies().asLiveData()
     override fun onClickMovie(movieNowPlayingResult: MovieNowPlayingResult) {
-
+        TODO("Not yet implemented")
     }
+
 
 //    override fun onClickCategory(genre: Genre) {
 ////        genreId.postValue(genre)
