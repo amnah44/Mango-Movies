ee868388e3a3c8a5c13988b6e3b6698d30ca3e9e
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-06 11:20:26 +0300

    create genre key mutable map to get the id of tv by movie id

--- app/src/main/java/com/mango/movies/util/Constant.kt
+++ app/src/main/java/com/mango/movies/util/Constant.kt
@@ -6,6 +6,26 @@ object Constant {
     const val BASE_PERSON_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
     const val api_key = "68ff47a440b5f644e1533eb5afbd1669"
 
+    val GENRE_KEY= mutableMapOf(28 to 10759,
+        12 to 10759,
+        16 to 16,
+        35 to 35,
+        80 to 80,
+        99 to 99,
+        18 to 18,
+        10751 to 10751,
+        14 to 18,
+        36 to 18,
+        27 to 18,
+        10402 to 18,
+        9648 to 9648,
+        10749 to 18,
+        878 to 18,
+        10770 to 18,
+        53 to 18,
+        10752 to 10768,
+        37 to 37)
+
     //Log tags to be used inside the fragments classes
     const val HOME_FRAGMENT = "Home Fragment"
     const val DETAILS_FRAGMENT = "Details Fragment"
