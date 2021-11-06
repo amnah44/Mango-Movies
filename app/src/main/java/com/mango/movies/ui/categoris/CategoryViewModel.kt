c18971889462106bdb13172eaf4db8713ada5f64
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-06 11:11:14 +0300

    initial variables in category view model

--- app/src/main/java/com/mango/movies/ui/categoris/CategoryViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/CategoryViewModel.kt
@@ -1,6 +1,19 @@
 package com.mango.movies.ui.categoris
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
+import com.mango.movies.model.domain.genre.Genre
+import com.mango.movies.util.State
 
 class CategoryViewModel() : ViewModel() {
+    var requiredGenre: Genre?=null
+    var flag=true
+    val  genreMovieList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
+
+    init {
+        requiredGenre=Genre(28,"action")
+    }
+
+
 }
