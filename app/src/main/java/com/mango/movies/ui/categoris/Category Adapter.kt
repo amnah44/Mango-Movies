0d1cced0df5259a7f365b5b40f37fdb273c080d7
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-06 10:46:35 +0300

    create category adapter

--- app/src/main/java/com/mango/movies/ui/categoris/Category Adapter.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/Category Adapter.kt
@@ -4,7 +4,13 @@ import com.mango.movies.R
 import com.mango.movies.ui.base.BaseAdapter
 import com.mango.movies.ui.base.BaseInteractionListener
 
+class CategoryAdapter(
+    item:List<Result>,
+    listener:CategoryInteractionListener
+):BaseAdapter<Result>(item,listener){
+    override val layoutId: Int = R.layout.item_category
 
+}
 
 
 interface CategoryInteractionListener: BaseInteractionListener {
