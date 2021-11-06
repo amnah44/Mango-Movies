ee0d4804e12b70007577c1fdd37b11a1cd741873
Amnah Ali <amnah1li@gmail.com>
2021-11-06 17:06:00 +0300

    convert item name of now playing to upcoming

--- app/src/main/java/com/mango/movies/ui/home/UpComingMovieAdapter.kt
+++ app/src/main/java/com/mango/movies/ui/home/UpComingMovieAdapter.kt
@@ -1,8 +1,6 @@
 package com.mango.movies.ui.home
 
 import com.mango.movies.R
-import com.mango.movies.model.domain.movie.nowPlaying.MovieNowPlayingResult
-import com.mango.movies.model.domain.movie.popular.MoviePopularResult
 import com.mango.movies.model.domain.movie.upComing.MovieUpComingResult
 import com.mango.movies.ui.base.BaseAdapter
 
@@ -10,6 +8,6 @@ class UpComingMovieAdapter(
     item: List<MovieUpComingResult>,
     listener: HomeInteractionListener
 ) : BaseAdapter<MovieUpComingResult>(item, listener) {
-    override val layoutId: Int = R.layout.item_now_playing_movie
+    override val layoutId: Int = R.layout.item_up_coming_movie
     override fun getItemCount(): Int = 8
 }
