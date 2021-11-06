13a77848c91b0f861d016d44d56d45e770f71760
basheer574 <spyderme99100@gmail.com>
2021-11-06 21:36:15 +0300

    enhance Person Fragment code

--- app/src/main/java/com/mango/movies/ui/person/PersonFragment.kt
+++ app/src/main/java/com/mango/movies/ui/person/PersonFragment.kt
@@ -22,8 +22,11 @@ class PersonFragment : BaseFragment<FragmentPersonBinding>(R.layout.fragment_per
 
 
     override fun setupView() {
-        binding.viewModel = viewModel
-        binding.personRecyclerView.adapter = PersonAdapter(mutableListOf(), viewModel)
+        binding.apply {
+            viewModel = viewModel
+            personRecyclerView.adapter = PersonAdapter(mutableListOf(), viewModel)
+        }
+
 
         viewModel.personDetails.observe(viewLifecycleOwner) {
             if(it!=null){
