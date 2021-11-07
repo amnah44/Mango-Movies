2c7dc2343899a0ba90f65138a597d6a038b32e3c
basheer574 <spyderme99100@gmail.com>
2021-11-07 10:37:37 +0300

    add progress text to progress indicator

--- app/src/main/java/com/mango/movies/ui/person/PersonAdapter.kt
+++ app/src/main/java/com/mango/movies/ui/person/PersonAdapter.kt
@@ -5,7 +5,7 @@ import com.mango.movies.ui.base.BaseAdapter
 import com.mango.movies.R
 import com.mango.movies.model.domain.person.popular.PersonPopularResult
 
-class PersonAdapter(items: List<PersonPopularResult>, listener: PersonInteractionListener) :
+class PersonAdapter(items: List<PersonPopularResult>, listener: PersonInteractionListener?) :
     BaseAdapter<PersonPopularResult>(items, listener) {
     override val layoutId = R.layout.item_people
 }
