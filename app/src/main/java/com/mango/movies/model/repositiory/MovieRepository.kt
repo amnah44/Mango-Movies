47b3880caaf0db16c465928bc09084d8e68f4d14
Amnah Ali <amnah1li@gmail.com>
2021-11-07 13:58:25 +0300

    add tv trending function to repository

--- app/src/main/java/com/mango/movies/model/repositiory/MovieRepository.kt
+++ app/src/main/java/com/mango/movies/model/repositiory/MovieRepository.kt
@@ -2,7 +2,6 @@ package com.mango.movies.model.repositiory
 
 import com.mango.movies.model.domain.person.details.PersonDetailsResponse
 import com.mango.movies.model.domain.person.popular.PersonPopularResponse
-import com.mango.movies.model.domain.person.popular.PersonPopularResult
 import com.mango.movies.model.domain.searchResponse.SearchResponse
 import com.mango.movies.model.network.API
 import com.mango.movies.util.Constant
@@ -33,33 +32,62 @@ object MovieRepository {
 
 
     fun movieDetails(movieId: Int) =
-        wrapWithFlow { API.apiService.getMovieDetails(movieId, Constant.api_key) }
+        wrapWithFlow {
+            API.apiService.getMovieDetails(movieId, Constant.api_key)
+        }
 
-    fun latestMovies() = wrapWithFlow { API.apiService.getLatestMovies(Constant.api_key,"en-US") }
-    fun nowPlayingMovies() = wrapWithFlow { API.apiService.getNowPlayingMovies(Constant.api_key) }
-    fun popularMovies() = wrapWithFlow { API.apiService.getPopularMovies(Constant.api_key) }
-    fun ratedMovies() = wrapWithFlow { API.apiService.getTopRatedMovies(Constant.api_key) }
-    fun upcomingMovies() = wrapWithFlow { API.apiService.getUpcomingMovies(Constant.api_key) }
+    fun latestMovies() = wrapWithFlow {
+        API.apiService.getLatestMovies(Constant.api_key, "en-US")
+    }
 
+    fun nowPlayingMovies() = wrapWithFlow {
+        API.apiService.getNowPlayingMovies(Constant.api_key)
+    }
 
+    fun popularMovies() = wrapWithFlow {
+        API.apiService.getPopularMovies(Constant.api_key)
+    }
+
+    fun ratedMovies() = wrapWithFlow {
+        API.apiService.getTopRatedMovies(Constant.api_key)
+    }
+
+    fun upcomingMovies() = wrapWithFlow {
+        API.apiService.getUpcomingMovies(Constant.api_key)
+    }
 
     fun tvShowDetails(tvId: Int) =
-        wrapWithFlow { API.apiService.getTvShowDetails(tvId, Constant.api_key) }
+        wrapWithFlow {
+            API.apiService.getTvShowDetails(tvId, Constant.api_key)
+        }
 
-    fun popularTvShow() = wrapWithFlow { API.apiService.getLatestTvShow(Constant.api_key) }
-    fun topRatedTvShow() = wrapWithFlow { API.apiService.getTopRatedTvShow(Constant.api_key) }
+    fun popularTvShow() = wrapWithFlow {
+        API.apiService.getLatestTvShow(Constant.api_key)
+    }
 
+    fun topRatedTvShow() = wrapWithFlow {
+        API.apiService.getTopRatedTvShow(Constant.api_key)
+    }
 
-    fun searchMovie(query: String): Flow<State<SearchResponse?>> =
-        wrapWithFlow { API.apiService.searchMovie(query, Constant.api_key) }
+    fun tvTrending() = wrapWithFlow {
+        API.apiService.getTrendingTV(Constant.api_key, "tv", "week")
+    }
+
+    fun searchMovie(query: String): Flow<State<SearchResponse?>> = wrapWithFlow {
+        API.apiService.searchMovie(query, Constant.api_key)
+    }
 
     fun personPopular(): Flow<State<PersonPopularResponse?>> =
-        wrapWithFlow { API.apiService.getPersonPopular(Constant.api_key) }
-
+        wrapWithFlow {
+            API.apiService.getPersonPopular(Constant.api_key)
+        }
 
     fun personDetails(personId: Int): Flow<State<PersonDetailsResponse?>> =
-        wrapWithFlow { API.apiService.getPersonDetails(personId, Constant.api_key) }
+        wrapWithFlow {
+            API.apiService.getPersonDetails(personId, Constant.api_key)
+        }
 
-
-    fun genres() = wrapWithFlow { API.apiService.getGenre(Constant.api_key) }
+    fun genres() = wrapWithFlow {
+        API.apiService.getGenre(Constant.api_key)
+    }
 }
