62377a2ef9241e3b93c0fe8d46b169ef5d37cee4
Amnah Ali <amnah1li@gmail.com>
2021-11-07 16:05:34 +0300

    add scroller to home fragment

--- app/src/main/java/com/mango/movies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeFragment.kt
@@ -20,6 +20,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         binding.recyclerUpComingMovies.adapter = UpComingMovieAdapter(mutableListOf(), viewModel)
         binding.recyclerTopRateMovies.adapter = TopRateMovieAdapter(mutableListOf(), viewModel)
         binding.recyclerTrendingSeries.adapter = TrendingSeriesAdapter(mutableListOf(), viewModel)
+        binding.recyclerTrendingMovie.adapter = TrendingSeriesAdapter(mutableListOf(), viewModel)
+
     }
 
 }
