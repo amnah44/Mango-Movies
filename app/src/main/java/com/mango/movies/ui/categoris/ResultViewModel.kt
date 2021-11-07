7b47a8f8858dfdd964d78288b8c1d9e9176eb505
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-07 13:31:16 +0300

    handle switch between movie and tv

--- app/src/main/java/com/mango/movies/ui/categoris/ResultViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/ResultViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
+import com.mango.movies.model.domain.genre.GenerResponse
 import com.mango.movies.model.domain.genre.Genre
 import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.util.State
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -15,12 +17,13 @@ class ResultViewModel() : ViewModel(),ResultInteractionListener ,GenreInteractio
     var requiredGenre: Genre?=null
     var flag=true
     val  genreMovieList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
-    val genres = MovieRepository.genres(true).asLiveData()
+    val genres =MutableLiveData<State<GenerResponse?>>()
 
     val genreId=MutableLiveData<Genre>()
 
     init {
         requiredGenre=Genre(28,"action")
+        getGenre()
         getMovie()
     }
     fun getMovie(){
@@ -31,8 +34,21 @@ class ResultViewModel() : ViewModel(),ResultInteractionListener ,GenreInteractio
         }
     }
 
+    fun getGenre(){
+        viewModelScope.launch {
+            MovieRepository.genres(flag).collect {
+                genres.postValue(it)
+            }
+        }
+    }
+
+
+
+
+
     fun changeMovieOrSeries(){
         flag=flag?.not()
+        getGenre()
         getMovie()
     }
 
