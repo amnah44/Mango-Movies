ad08e6f21248328163a0e0b5d254748c14b19526
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-08 18:07:55 +0300

    refactor SearchInteractionListener

--- app/src/main/java/com/mango/movies/ui/search/SearchInteractionListener.kt
+++ app/src/main/java/com/mango/movies/ui/search/SearchInteractionListener.kt
@@ -1,7 +1,7 @@
 package com.mango.movies.ui.search
-import com.mango.movies.model.domain.searchResponse.Result
+import com.mango.movies.model.domain.Movie
 import com.mango.movies.ui.base.BaseInteractionListener
 
 interface SearchInteractionListener  : BaseInteractionListener {
-    fun onClickMovie(movie: Result)
+    fun onClickMovie(movie: Movie)
 }
