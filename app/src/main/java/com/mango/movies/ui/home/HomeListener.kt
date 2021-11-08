a6fb4efdb7746e081f072e8fb225417938bbea68
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-08 17:40:27 +0300

    refactor HomeListener

--- app/src/main/java/com/mango/movies/ui/home/HomeListener.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeListener.kt
@@ -1,12 +1,14 @@
 package com.mango.movies.ui.home
 
 import com.mango.movies.model.domain.Movie
+import com.mango.movies.model.domain.Series
 import com.mango.movies.ui.base.BaseInteractionListener
 
 interface HomeInteractionListener : BaseInteractionListener {
     fun onTopRateMoviesClick(movieTopRate: Movie)
-    fun onTopRateSeriesClick(seriesTopRate: Movie)
-    fun onTrendingClick(tvTrending: Movie)
     fun onUpComingMoviesClick(moviesUpComing: Movie)
+
+    fun onTopRateSeriesClick(seriesTopRate: Series)
+    fun onTrendingClick(tvTrending: Series)
 }
 
