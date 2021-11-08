17b4dbe9fa0b09ef91dd059bd818d6383f5ca7a5
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-08 18:05:50 +0300

    refactor MovieActivity

--- app/src/main/java/com/mango/movies/ui/MovieActivity.kt
+++ app/src/main/java/com/mango/movies/ui/MovieActivity.kt
@@ -14,6 +14,7 @@ class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+
     }
 
     override fun onResume() {
