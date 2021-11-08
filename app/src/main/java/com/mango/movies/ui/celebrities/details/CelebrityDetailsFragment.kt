ee3dd4ec68d6dcbedabdbb83f498929030b41f9b
basheer574 <spyderme99100@gmail.com>
2021-11-08 17:50:03 +0300

    fix naming

--- app/src/main/java/com/mango/movies/ui/celebrities/details/CelebrityDetailsFragment.kt
+++ app/src/main/java/com/mango/movies/ui/celebrities/details/CelebrityDetailsFragment.kt
@@ -7,21 +7,20 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.mango.movies.R
-import com.mango.movies.databinding.FragmentPersonDetailsBinding
+import com.mango.movies.databinding.FragmentCelebrityDetailsBinding
 import com.mango.movies.ui.base.BaseFragment
-import com.mango.movies.ui.celebrities.CelebrityFragmentArgs
 import com.mango.movies.ui.celebrities.CelebrityPopularViewModel
 import com.mango.movies.util.Constant
 
-class PersonDetailsFragment :
-    BaseFragment<FragmentPersonDetailsBinding>(R.layout.fragment_person_details) {
+class CelebrityDetailsFragment :
+    BaseFragment<FragmentCelebrityDetailsBinding>(R.layout.fragment_celebrity_details) {
 
     override val viewModel = CelebrityPopularViewModel()
     override val LOG_TAG: String = Constant.PERSON_DETAILS_FRAGMENT
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentPersonDetailsBinding =
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentCelebrityDetailsBinding =
         DataBindingUtil::inflate
 
-    private val args: CelebrityFragmentArgs by navArgs()
+    private val args: CelebrityDetailsFragmentArgs by navArgs()
 
     override fun setupView() {
         binding.item = args.celebrityDetails
