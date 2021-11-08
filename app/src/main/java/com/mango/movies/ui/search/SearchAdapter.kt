c88e810a2890aad12fc07e5661a905947d832c90
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-08 18:06:38 +0300

    refactor SearchAdapter

--- app/src/main/java/com/mango/movies/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/mango/movies/ui/search/SearchAdapter.kt
@@ -1,8 +1,8 @@
 package com.mango.movies.ui.search
 import com.mango.movies.R
-import com.mango.movies.model.domain.searchResponse.Result
+import com.mango.movies.model.domain.Movie
 import com.mango.movies.ui.base.BaseAdapter
 
-class SearchAdapter(items: List<Result>, listener: SearchInteractionListener) : BaseAdapter<Result>(items, listener) {
+class SearchAdapter(items: List<Movie>, listener: SearchInteractionListener) : BaseAdapter<Movie>(items, listener) {
     override val layoutId: Int = R.layout.item_search
 }
