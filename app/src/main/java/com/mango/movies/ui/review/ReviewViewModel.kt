86c6e09e5dfc428729ddd336117b117ea5447b2a
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-09 02:09:28 +0300

    add review as live data in review view model

--- app/src/main/java/com/mango/movies/ui/review/ReviewViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/review/ReviewViewModel.kt
@@ -1,6 +1,9 @@
 package com.mango.movies.ui.review
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.mango.movies.model.repositiory.ReviewRepository
 
 class ReviewViewModel: ViewModel() {
+    val review = ReviewRepository.movieReview().asLiveData()
 }
