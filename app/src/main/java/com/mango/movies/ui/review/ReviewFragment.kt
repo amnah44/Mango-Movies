7f850be8099980d1c0e2dab4436240639416d2dc
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-09 00:30:49 +0300

    add review fragment class and xml file

---
+++ app/src/main/java/com/mango/movies/ui/review/ReviewFragment.kt
@@ -0,0 +1,21 @@
+package com.mango.movies.ui.review
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import com.mango.movies.R
+import com.mango.movies.databinding.FragmentReviewBinding
+import com.mango.movies.ui.base.BaseFragment
+
+class ReviewFragment: BaseFragment<FragmentReviewBinding>(R.layout.fragment_review){
+    override val LOG_TAG: String
+        get() = TODO("Not yet implemented")
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentReviewBinding
+        get() = TODO("Not yet implemented")
+
+    override fun setupView() {
+        TODO("Not yet implemented")
+    }
+}
