c455575819b0f6e22fb6d3e42ba136d105b205fc
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-08 18:08:06 +0300

    refactor SearchViewModel

--- app/src/main/java/com/mango/movies/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/search/SearchViewModel.kt
@@ -2,43 +2,34 @@ package com.mango.movies.ui.search
 
 
 import android.util.Log
-import androidx.appcompat.widget.SearchView
-import androidx.databinding.adapters.SearchViewBindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.mango.movies.model.domain.searchResponse.Result
-import com.mango.movies.model.domain.searchResponse.SearchResponse
+import com.mango.movies.model.domain.BaseResponse
+import com.mango.movies.model.domain.Movie
+
 import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.util.State
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.collect
 
-class SearchViewModel : ViewModel(), SearchInteractionListener {
-    var searchResult = MutableLiveData<State<SearchResponse?>?>()
-    var selectedMovie =  MutableLiveData<Result?>()
 
-    override fun onClickMovie(movie: Result) {
+class SearchViewModel : ViewModel(), SearchInteractionListener {
+    var searchResult = MutableLiveData<State<BaseResponse<Movie>?>?>()
+    var selectedMovie =  MutableLiveData<Movie?>()
+
+    override fun onClickMovie(movie: Movie) {
         Log.v("ALI",movie.title.toString())
         selectedMovie.postValue(movie)
     }
 
-    val onQueryTextListener: SearchView.OnQueryTextListener =
-        object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                Log.v("Test", "query = $query")
-                viewModelScope.launch {
-                    MovieRepository.searchMovie(query).collect {
-                        searchResult.postValue(it)
-                    }
-                }
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String): Boolean {
-                return false
+    fun onTextChanged(text: CharSequence?) {
+        // TODO do something with text
+        viewModelScope.launch {
+            MovieRepository.searchMovie(text.toString()).collect {
+                searchResult.postValue(it)
             }
         }
+    }
 
 }
