305c28677e3b049aade97b711572fa1b67f2a782
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-11-08 18:01:40 +0300

    refactor Series model

--- app/src/main/java/com/mango/movies/model/domain/Series.kt
+++ app/src/main/java/com/mango/movies/model/domain/Series.kt
@@ -31,7 +31,7 @@ data class Series(
     @SerializedName("poster_path")
     val posterPath: String?,
     @SerializedName("vote_average")
-    val voteAverage: Double?,
+    val voteAverage: Float?,
     @SerializedName("vote_count")
     val voteCount: Int?
 ):Parcelable
