bfbcffce12c6c1b357f0455d1cccdb14f9f33825
Amnah Ali <amnah1li@gmail.com>
2021-11-08 09:21:49 +0300

    add another listener to home view model

--- app/src/main/java/com/mango/movies/ui/home/HomeListener.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeListener.kt
@@ -1,11 +1,15 @@
 package com.mango.movies.ui.home
 
 import com.mango.movies.model.domain.movie.topRated.MovieTopRatedResult
+import com.mango.movies.model.domain.movie.upComing.MovieUpComingResult
+import com.mango.movies.model.domain.tv.topRated.TVTopRatedResult
 import com.mango.movies.model.domain.tv.trending.TVTrendingResult
 import com.mango.movies.ui.base.BaseInteractionListener
 
 interface HomeInteractionListener : BaseInteractionListener {
-    fun onTopRateClick(movieTopRate: MovieTopRatedResult)
+    fun onTopRateMoviesClick(movieTopRate: MovieTopRatedResult)
+    fun onTopRateSeriesClick(seriesTopRate: TVTopRatedResult)
     fun onTrendingClick(tvTrending: TVTrendingResult)
+    fun onUpComingMoviesClick(moviesUpComing: MovieUpComingResult)
 }
 
--- app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/home/HomeViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.navigation.NavDirections
 import com.mango.movies.model.domain.movie.topRated.MovieTopRatedResult
+import com.mango.movies.model.domain.movie.upComing.MovieUpComingResult
+import com.mango.movies.model.domain.tv.topRated.TVTopRatedResult
 import com.mango.movies.model.domain.tv.trending.TVTrendingResult
 import com.mango.movies.model.repositiory.MovieRepository
 
@@ -13,11 +15,20 @@ class HomeViewModel : ViewModel(), HomeInteractionListener {
     val topRateMovies = MovieRepository.ratedMovies().asLiveData()
     val trendingTV = MovieRepository.tvTrending().asLiveData()
     val topRateSeries = MovieRepository.topRatedTvShow().asLiveData()
-    override fun onTopRateClick(movieTopRate: MovieTopRatedResult) {
+
+    override fun onTopRateMoviesClick(movieTopRate: MovieTopRatedResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTopRateSeriesClick(seriesTopRate: TVTopRatedResult) {
         TODO("Not yet implemented")
     }
 
     override fun onTrendingClick(tvTrending: TVTrendingResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpComingMoviesClick(moviesUpComing: MovieUpComingResult) {
         TODO("Not yet implemented")
     }
 
