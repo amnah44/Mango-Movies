b975505ea6a30f84ec06b689b5358059f0c7e742
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-10 00:35:28 +0300

    add search review adapter and interaction listener

---
+++ app/src/main/java/com/mango/movies/ui/review/searchReview/SearchReviewAdapter.kt
@@ -0,0 +1,15 @@
+package com.mango.movies.ui.review.searchReview
+
+import com.mango.movies.R
+import com.mango.movies.model.domain.review.ReviewResult
+import com.mango.movies.ui.base.BaseAdapter
+import com.mango.movies.ui.base.BaseInteractionListener
+
+class SearchReviewAdapter(
+    item: List<ReviewResult>,
+    listener: SearchReviewInteractionListener?
+) : BaseAdapter<ReviewResult>(item, listener) {
+    override val layoutId: Int = R.layout.item_search
+}
+
+interface SearchReviewInteractionListener: BaseInteractionListener
