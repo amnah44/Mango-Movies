9cd9423f87959178f5822d50329e624c30f86763
Amnah Ali <amnah1li@gmail.com>
2021-11-09 20:47:10 +0300

    remove unusable constant

--- app/src/main/java/com/mango/movies/util/Constant.kt
+++ app/src/main/java/com/mango/movies/util/Constant.kt
@@ -2,7 +2,6 @@ package com.mango.movies.util
 
 object Constant {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val BASE_MOVIE_URL = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"
     const val BASE_PERSON_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
     const val api_key = "68ff47a440b5f644e1533eb5afbd1669"
     const val BASE_REVIEW_URL = "https://api.nytimes.com//svc/movies/v2/"
@@ -14,9 +13,6 @@ object Constant {
     const val PEOPLE_FRAGMENT = "People Fragment"
     const val CATEGORY_FRAGMENT = "Category Fragment"
     const val PERSON_DETAILS_FRAGMENT = "Person Details Fragment"
-    const val SHOW_MORE_MOVIES_FRAGMENT = "Show More Movies Fragment"
-    const val SHOW_MORE_SERIES_FRAGMENT = "Show More Series Fragment"
-    const val FAVORITES_FRAGMENT = "Favorites Fragment"
     const val SEARCH_FRAGMENT = "Search Fragment"
     const val SETTINGS_FRAGMENT = "Settings Fragment"
     const val REVIEW_FRAGMENT = "Review Fragment"
