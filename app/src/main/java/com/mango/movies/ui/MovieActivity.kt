96a471796d3b57978ff61855584955b4dbf0e40a
basheer574 <spyderme99100@gmail.com>
2021-11-09 10:33:16 +0300

    add theme listener

--- app/src/main/java/com/mango/movies/ui/MovieActivity.kt
+++ app/src/main/java/com/mango/movies/ui/MovieActivity.kt
@@ -1,7 +1,10 @@
 package com.mango.movies.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -14,13 +17,24 @@ class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
-
+        changeTheme()
     }
 
     override fun onResume() {
         super.onResume()
-
         val navController = findNavController(R.id.parent_container)
         _binding.bottomNav.setupWithNavController(navController)
+    }
+    private fun changeTheme(){
+        _binding.imageTheme.setOnClickListener{
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                Configuration.UI_MODE_NIGHT_NO ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+        }
     }
 }
