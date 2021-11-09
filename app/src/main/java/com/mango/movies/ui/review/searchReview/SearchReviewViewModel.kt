3ea4a1e09eea2ddf6f2e8bbabd6458f0106ada9e
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-09 23:53:49 +0300

    add on text change fun to search review voew model

--- app/src/main/java/com/mango/movies/ui/review/searchReview/SearchReviewViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/review/searchReview/SearchReviewViewModel.kt
@@ -2,9 +2,23 @@ package com.mango.movies.ui.review.searchReview
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mango.movies.model.domain.review.ReviewResponse
+import com.mango.movies.model.repositiory.ReviewRepository
 import com.mango.movies.util.State
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class SearchReviewViewModel: ViewModel() {
     val searchReview = MutableLiveData<State<ReviewResponse?>?>()
+    val flag = MutableLiveData<Boolean>()
+
+    fun onTextChanged(text: CharSequence?) {
+        flag.postValue(false)
+        viewModelScope.launch{
+            ReviewRepository.searchMovieReview(text.toString()).collect{
+                searchReview.postValue(it)
+            }
+        }
+    }
 }
