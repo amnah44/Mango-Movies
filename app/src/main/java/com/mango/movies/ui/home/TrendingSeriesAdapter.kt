2610c5622e7abfe644a69c5176a9a383d83b80a1
AliJalil <Ali.Abdulzahraaj@gmail.com>
2021-11-09 11:34:10 +0300

    refactor home fragment component

--- app/src/main/java/com/mango/movies/ui/home/TrendingSeriesAdapter.kt
+++ app/src/main/java/com/mango/movies/ui/home/TrendingSeriesAdapter.kt
@@ -3,10 +3,11 @@ package com.mango.movies.ui.home
 import com.mango.movies.R
 import com.mango.movies.model.domain.Series
 import com.mango.movies.ui.base.BaseAdapter
+import com.mango.movies.ui.tv.series.SeriesInteractionListener
 
 class TrendingSeriesAdapter(
     item: List<Series>,
-    listener: HomeInteractionListener
+    listener: SeriesInteractionListener
 ) : BaseAdapter<Series>(item, listener) {
     override val layoutId: Int = R.layout.item_trending_series
 }
