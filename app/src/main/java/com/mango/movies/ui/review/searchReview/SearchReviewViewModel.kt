4faabedfd22c7864ae3d81026ef5ebdd6f5176da
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-09 23:37:09 +0300

    add search review as live data

--- app/src/main/java/com/mango/movies/ui/review/searchReview/SearchReviewViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/review/searchReview/SearchReviewViewModel.kt
@@ -1,6 +1,10 @@
 package com.mango.movies.ui.review.searchReview
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mango.movies.model.domain.review.ReviewResponse
+import com.mango.movies.util.State
 
 class SearchReviewViewModel: ViewModel() {
+    val searchReview = MutableLiveData<State<ReviewResponse?>?>()
 }
