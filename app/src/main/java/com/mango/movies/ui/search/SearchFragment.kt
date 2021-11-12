a658fc1026c787a10d4f8599866ff0993ab36bfd
basheer574 <spyderme99100@gmail.com>
2021-11-12 14:10:15 +0300

    search view model done

--- app/src/main/java/com/mango/movies/ui/search/SearchFragment.kt
+++ app/src/main/java/com/mango/movies/ui/search/SearchFragment.kt
@@ -3,6 +3,7 @@ package com.mango.movies.ui.search
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -13,7 +14,7 @@ import com.mango.movies.util.Constant
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override val LOG_TAG: String = Constant.SEARCH_FRAGMENT
-    override val viewModel: SearchViewModel by viewModels()
+    override val viewModel by activityViewModels<SearchViewModel>()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentSearchBinding =
         DataBindingUtil::inflate
 
