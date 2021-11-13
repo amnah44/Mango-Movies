96ca0f5af8baaa928848f8dcc6403175d005976e
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-13 17:52:20 +0300

    set view models

--- app/src/main/java/com/mango/movies/ui/categoris/MovieTabFragment.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/MovieTabFragment.kt
@@ -3,6 +3,7 @@ package com.mango.movies.ui.categoris
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import com.mango.movies.R
 import com.mango.movies.databinding.FragmentTabMovieBinding
@@ -11,8 +12,7 @@ import com.mango.movies.util.Constant
 
 class MovieTabFragment:BaseFragment<FragmentTabMovieBinding>(R.layout.fragment_tab_movie){
     override val LOG_TAG: String= Constant.MOVIE_TAB_FRAGMENT
-    override val viewModel: ViewModel
-        get() = TODO("Not yet implemented")
+    override val viewModel: MovieViewModel by viewModels()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentTabMovieBinding=DataBindingUtil::inflate
 
     override fun setupView() {
--- app/src/main/java/com/mango/movies/ui/categoris/SeriesTabFragment.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/SeriesTabFragment.kt
@@ -3,6 +3,7 @@ package com.mango.movies.ui.categoris
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import com.mango.movies.R
 import com.mango.movies.databinding.FragmentTabSeriesBinding
@@ -11,8 +12,7 @@ import com.mango.movies.util.Constant
 
 class SeriesTabFragment:BaseFragment<FragmentTabSeriesBinding>(R.layout.fragment_tab_series) {
     override val LOG_TAG: String= Constant.SERIES_TAB_FRAGMENT
-    override val viewModel: ViewModel
-        get() = TODO("Not yet implemented")
+    override val viewModel:SeriesViewModel by viewModels()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentTabSeriesBinding=DataBindingUtil::inflate
 
     override fun setupView() {
