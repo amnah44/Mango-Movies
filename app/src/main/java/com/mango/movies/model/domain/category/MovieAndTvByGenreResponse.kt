ddf081633b44c93d9956a00389f242fa3ba2fb64
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-13 20:44:07 +0300

    make category domain nullable

--- app/src/main/java/com/mango/movies/model/domain/category/MovieAndTvByGenreResponse.kt
+++ app/src/main/java/com/mango/movies/model/domain/category/MovieAndTvByGenreResponse.kt
@@ -3,9 +3,9 @@ package com.mango.movies.model.domain.category
 import com.mango.movies.model.domain.category.Result
 
 data class MovieAndTvByGenreResponse(
-    val id: Int,
-    val page: Int,
-    val results: List<Result>,
-    val total_pages: Int,
-    val total_results: Int
+    val id: Int?,
+    val page: Int?,
+    val results: List<Result>?,
+    val total_pages: Int?,
+    val total_results: Int?
 )
