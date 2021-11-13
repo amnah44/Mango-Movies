a7ab8b568f98e79371f4a50fcf45c4be98390157
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-13 19:20:31 +0300

    set on tab selected listener

--- app/src/main/java/com/mango/movies/ui/categoris/CategoryFragment.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/CategoryFragment.kt
@@ -1,11 +1,13 @@
 package com.mango.movies.ui.categoris
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import com.google.android.material.tabs.TabLayout
 import com.mango.movies.R
 import com.mango.movies.databinding.FragmentCategoryBinding
 import com.mango.movies.ui.base.BaseFragment
@@ -27,6 +29,21 @@ class CategoryFragment : BaseFragment<FragmentCategoryBinding>(R.layout.fragment
         val seriesTab=binding.myTabsLayout.newTab()
         seriesTab.setText("series")
         binding.myTabsLayout.addTab(seriesTab)
+
+        binding.myTabsLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Log.i("hddd","gggg")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Log.i("hddd","gggg")
+            }
+
+        })
 
     }
 }
