e560d80f2e16be0d274d5dd48148d662869b322e
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-13 17:39:50 +0300

    handle genre click listener

--- app/src/main/java/com/mango/movies/ui/categoris/MovieViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/MovieViewModel.kt
@@ -3,6 +3,7 @@ package com.mango.movies.ui.categoris
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.mango.movies.model.domain.Movie
 import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
 import com.mango.movies.model.domain.genre.GenerResponse
@@ -10,12 +11,27 @@ import com.mango.movies.model.domain.genre.Genre
 import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.util.Event
 import com.mango.movies.util.State
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class MovieViewModel:ViewModel() {
+class MovieViewModel:ViewModel(), GenreInteractionListener{
     val selectedMovieEvent = MutableLiveData<Event<State<Movie?>>>()
     val  genreMovieList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
     val genres =MovieRepository.genres(true).asLiveData()
     var requiredGenre=Genre(28,"action")
 
-    
+    fun getMovie(){
+        viewModelScope.launch {
+            MovieRepository.getGenreMovieOrTv(requiredGenre?.id, true).collect {
+                genreMovieList.postValue(it)
+            }
+        }
+    }
+
+    override fun onClickCategory(genre: Genre) {
+        requiredGenre=genre
+        getMovie()
+    }
+
+
 }
--- app/src/main/java/com/mango/movies/ui/categoris/SeriesViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/SeriesViewModel.kt
@@ -3,17 +3,37 @@ package com.mango.movies.ui.categoris
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.mango.movies.model.domain.Series
 import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
 import com.mango.movies.model.domain.genre.Genre
 import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.util.Event
 import com.mango.movies.util.State
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class SeriesViewModel:ViewModel() {
+class SeriesViewModel:ViewModel() ,GenreInteractionListener{
     val selectedSeriesEvent = MutableLiveData<Event<State<Series?>>>()
     val  genreSeriesList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
     val genres = MovieRepository.genres(false).asLiveData()
     var requiredGenre=Genre(10759,"action")
+
+    init {
+
+        getSeries()
+    }
+    fun getSeries(){
+        viewModelScope.launch {
+            MovieRepository.getGenreMovieOrTv(requiredGenre?.id, false).collect {
+                genreSeriesList.postValue(it)
+            }
+        }
+    }
+
+    override fun onClickCategory(genre: Genre) {
+        requiredGenre=genre
+        getSeries()
+    }
 
 }
