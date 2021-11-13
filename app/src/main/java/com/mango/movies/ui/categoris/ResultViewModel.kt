91fc3b648ba9d72fc5732b710f69ae8e2f9639bd
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-13 16:46:10 +0300

    init series and movie view model

--- app/src/main/java/com/mango/movies/ui/categoris/ResultViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/ResultViewModel.kt
@@ -18,66 +18,6 @@ import com.mango.movies.model.domain.category.Result
 import com.mango.movies.model.domain.details.DetailsResponse
 import com.mango.movies.util.Event
 
-class ResultViewModel() : ViewModel(),ResultInteractionListener ,GenreInteractionListener{
-    val  genreMovieList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
-    val genres =MutableLiveData<State<GenerResponse?>>()
-    val selectedMovieEvent = MutableLiveData<Event<State<Movie?>>>()
-    val selectedSeriesEvent = MutableLiveData<Event<State<Series?>>>()
-    var requiredGenre: Genre?=null
-    var flag=true
-
-    init {
-        getGenre()
-        getMovie()
-    }
-    private fun getMovie(){
-        viewModelScope.launch {
-            MovieRepository.getGenreMovieOrTv(requiredGenre?.id,flag).collect {
-                genreMovieList.postValue(it)
-            }
-        }
-    }
-
-    private fun getGenre(){
-        viewModelScope.launch {
-            MovieRepository.genres(flag).collect {
-                genres.postValue(it)
-            }
-        }
-    }
-
-    fun changeMovieOrSeries(switchFlag:Boolean){
-        flag=switchFlag
-        requiredGenre = if (flag)
-            Genre(28,"action")
-        else
-            Genre(10759,"action")
-        getGenre()
-        getMovie()
-    }
-
-    override fun onClickItem(result: Result) {
-        if(flag){
-           viewModelScope.launch {
-               MovieRepository.movieDetails(result.id).collect {
-                   selectedMovieEvent.postValue(Event(it))
-               }
-           }
-        }
-
-        else{
-            viewModelScope.launch {
-                MovieRepository.tvShowDetails(result.id).collect {
-                    selectedSeriesEvent.postValue(Event(it))
-                }
-            }
-        }
-
-    }
-
-    override fun onClickCategory(genre: Genre) {
-        requiredGenre=genre
-        getMovie()
-    }
+class ResultViewModel() : ViewModel(){
 
 }
--- app/src/main/java/com/mango/movies/ui/categoris/SeriesViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/SeriesViewModel.kt
@@ -1,6 +1,15 @@
 package com.mango.movies.ui.categoris
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mango.movies.model.domain.Series
+import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
+import com.mango.movies.model.domain.genre.Genre
+import com.mango.movies.util.Event
+import com.mango.movies.util.State
 
 class SeriesViewModel:ViewModel() {
+    val selectedSeriesEvent = MutableLiveData<Event<State<Series?>>>()
+    val  genreMovieList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
+    var requiredGenre: Genre?=null
 }
