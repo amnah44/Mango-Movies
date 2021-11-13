53bc6a75ad47822d6811af338c6bd9f932642af6
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-13 15:41:14 +0300

    encapsulate variables in details series view model

--- app/src/main/java/com/mango/movies/ui/series/DetailsSeriesViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/series/DetailsSeriesViewModel.kt
@@ -1,5 +1,6 @@
 package com.mango.movies.ui.series
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,18 +13,22 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class DetailsSeriesViewModel: ViewModel(), SeriesInteractionListener {
-    var similarSeries = MutableLiveData<State<BaseResponse<Series>?>?>()
-    var selectedSeriesEvent =  MutableLiveData<Event<Series>>()
+    private var _similarSeries = MutableLiveData<State<BaseResponse<Series>?>?>()
+    val similarSeries: LiveData<State<BaseResponse<Series>?>?>
+        get() = _similarSeries
+    private var _selectedSeriesEvent =  MutableLiveData<Event<Series>>()
+    val selectedSeriesEvent: LiveData<Event<Series>>
+        get() = _selectedSeriesEvent
 
     fun getSimilarSeries(seriesId: Int) {
         viewModelScope.launch {
             Repository.getSimilarSeries(seriesId).collect {
-                similarSeries.postValue(it)
+                _similarSeries.postValue(it)
             }
         }
     }
 
     override fun onSeriesClick(series: Series) {
-        selectedSeriesEvent.postValue(Event(series))
+        _selectedSeriesEvent.postValue(Event(series))
     }
 }
