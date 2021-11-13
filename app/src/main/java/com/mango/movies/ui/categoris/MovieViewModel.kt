f8e99c36b623d08f24c29dd4cbf4e30284a635db
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-13 17:29:44 +0300

    create genres request

--- app/src/main/java/com/mango/movies/ui/categoris/MovieViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/MovieViewModel.kt
@@ -2,15 +2,19 @@ package com.mango.movies.ui.categoris
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.mango.movies.model.domain.Movie
 import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
+import com.mango.movies.model.domain.genre.GenerResponse
 import com.mango.movies.model.domain.genre.Genre
+import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.util.Event
 import com.mango.movies.util.State
 
 class MovieViewModel:ViewModel() {
     val selectedMovieEvent = MutableLiveData<Event<State<Movie?>>>()
     val  genreMovieList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
+    val genres =MovieRepository.genres(true).asLiveData()
     var requiredGenre=Genre(28,"action")
 
     
--- app/src/main/java/com/mango/movies/ui/categoris/SeriesViewModel.kt
+++ app/src/main/java/com/mango/movies/ui/categoris/SeriesViewModel.kt
@@ -2,14 +2,18 @@ package com.mango.movies.ui.categoris
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.mango.movies.model.domain.Series
 import com.mango.movies.model.domain.category.MovieAndTvByGenreResponse
 import com.mango.movies.model.domain.genre.Genre
+import com.mango.movies.model.repositiory.MovieRepository
 import com.mango.movies.util.Event
 import com.mango.movies.util.State
 
 class SeriesViewModel:ViewModel() {
     val selectedSeriesEvent = MutableLiveData<Event<State<Series?>>>()
     val  genreSeriesList= MutableLiveData<State<MovieAndTvByGenreResponse?>>()
+    val genres = MovieRepository.genres(false).asLiveData()
     var requiredGenre=Genre(10759,"action")
+
 }
