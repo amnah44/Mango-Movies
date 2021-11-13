d427ed2da2b4d02bf74d354b107008b0b0a74758
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-14 01:55:47 +0300

    change view model scope of celebrity view model

--- app/src/main/java/com/mango/movies/ui/celebrities/details/CelebrityDetailsFragment.kt
+++ app/src/main/java/com/mango/movies/ui/celebrities/details/CelebrityDetailsFragment.kt
@@ -3,6 +3,7 @@ package com.mango.movies.ui.celebrities.details
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.mango.movies.R
@@ -14,7 +15,7 @@ import com.mango.movies.util.Constant
 class CelebrityDetailsFragment :
     BaseFragment<FragmentCelebrityDetailsBinding>(R.layout.fragment_celebrity_details) {
 
-    override val viewModel = CelebrityPopularViewModel()
+    override val viewModel by activityViewModels<CelebrityPopularViewModel>()
     override val LOG_TAG: String = Constant.PERSON_DETAILS_FRAGMENT
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentCelebrityDetailsBinding =
         DataBindingUtil::inflate
